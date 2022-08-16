eaacb190ffe36bdbfa7ba92a1ed3c4ea647c040a
Tommy Boileau <tommyboileau@Tommys-MacBook-Air.local>
2022-08-16 14:29:13 -0600

    started generating elements in the js file with comments on what needs to be done

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+//---------------------------
+//designate global variables
+var submitBtn = $("#submitButton");
+var movieCard = $(".movieCard");
+var navSelector = "";
+//---------------------------
+
+//----------------------------
+//select which elements will be hidden on initial page load
+randomelement.hide();
+//----------------------------
+
+//----------------------------
+//create a function to generate the movie cards based on the selected dropdown.
+function generateCards() {
+  //console log the selected genre
+  console.log(navSelector.val);
+
+  //take the genre and grab the genre code to pull from database
+}
+//----------------------------
+
+//-----------------------------
+//create a function to generate a modal containing the movie trailer pulled from youtube api
+function trailerModal() {}
+//---------------------------
+
+//---------------------------
+//add event listener to the movie card to run the trailerModal function to create a modal and display the youtube api corresponding movie trailer.
+movieCard.on("click", function () {
+  trailerModal();
+});
+//---------------------------
+
+//---------------------------
+//add event listener to the submit button on the top of the page to take the selected genre and run the generateCards function for that genre.
+submitBtn.on("click", function () {
+  generateCards();
+});
+//--------------------------
