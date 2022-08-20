671243865dd4f8a532a2e01205606dc3977b25f7
Kamary <kamarygillespie@Kamarys-MBP.lan>
2022-08-20 09:31:26 -0600

    added class to trailer button for better styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ var navSelector = $("#genrePicker");
 var cardholder = $("#cardholder");
 
 var youtubeUrl =
-  "https://www.googleapis.com/youtube/v3/search?key=AIzaSyBBpyA_FhpagYzYlPCI3Q440ghki-kAEPA&safeSearch=moderate&q=";
+    "https://www.googleapis.com/youtube/v3/search?key=AIzaSyBBpyA_FhpagYzYlPCI3Q440ghki-kAEPA&safeSearch=moderate&q=";
 
 var introPage = $("#intro-page");
 var videoModal = $("#videoModal");
@@ -48,36 +48,36 @@ var imgUrl = "https://image.tmdb.org/t/p/w500";
 //---------------------------
 //create a function to fetch movies from TMBD api
 function getMovies() {
-  $("#cardholder").empty();
-  var optionValue = $("#genrePicker").val();
-  var apiUrl = baseUrl + apiKey + "&with_genres=" + optionValue;
-  console.log(apiUrl);
+    $("#cardholder").empty();
+    var optionValue = $("#genrePicker").val();
+    var apiUrl = baseUrl + apiKey + "&with_genres=" + optionValue;
+    console.log(apiUrl);
 
-  fetch(apiUrl)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      showMovies(data);
-      cardholder.show();
-      introPage.hide();
-    });
+    fetch(apiUrl)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            showMovies(data);
+            cardholder.show();
+            introPage.hide();
+        });
 }
 //----------------------------
 
 //----------------------------
 //create a function to show movie info
 function showMovies(data) {
-  cardholder.innerHTML = "";
-  console.log(data.results);
-  data.results.forEach((movie) => {
-    var { title, poster_path, overview } = movie;
-    var movieEl = document.createElement("div");
-    movieEl.classList.add("movie");
-    movieEl.innerHTML = `
+    cardholder.innerHTML = "";
+    console.log(data.results);
+    data.results.forEach((movie) => {
+        var { title, poster_path, overview } = movie;
+        var movieEl = document.createElement("div");
+        movieEl.classList.add("movie");
+        movieEl.innerHTML = `
     <div class="card justify-content-center" style="min-height: 285px; width:575px;"
     
     >
-      <div class="row g-0">
+      <div class="row">
         <div class="col-md-4">
           <img src="${
             imgUrl + poster_path
@@ -86,11 +86,11 @@ function showMovies(data) {
       <div class="col-md-8">
         <div class="card-body">
           <h5 class="card-title">${title}</h5>
-          <p class="card-text">${overview}</p> <button alt=${title} data-bs-target="#videoModal"
+          <p class="card-text">${overview}</p> <button alt=${title} class="btn btn-outline-danger" data-bs-target="#videoModal"
     data-bs-toggle="modal">Click here for trailer</button>
         </div>`;
-    cardholder.append(movieEl);
-  });
+        cardholder.append(movieEl);
+    });
 }
 //---------------------------
 
@@ -102,62 +102,62 @@ cardholder.hide();
 //-----------------------------
 //create a function to generate a modal containing the movie trailer pulled from youtube api
 function trailerModal() {
-  //---------------------------
-  var src = "https://www.youtube.com/embed/dQw4w9WgXcQ";
-  $("#videoModal").show;
-  $("#videoModal iframe").attr("src", src);
+    //---------------------------
+    var src = "https://www.youtube.com/embed/dQw4w9WgXcQ";
+    $("#videoModal").show;
+    $("#videoModal iframe").attr("src", src);
 }
 //---------------------------
 //add event listener to the movie card to run the trailerModal function to create a modal and display the youtube api corresponding movie trailer.
 // $(".card").on("click", function () {
-$("#cardholder").on("click", ".card", function (e) {
-  console.log("test");
-  console.log(e.target);
-  // trailerModal();
+$("#cardholder").on("click", ".card", function(e) {
+    console.log("test");
+    console.log(e.target);
+    // trailerModal();
 });
 //---------------------------
 
 //---------------------------
 //add event listener to the submit button on the top of the page to take the selected genre and run the generateCards function for that genre. Also will dynamically update the h2 and h3 text in the intro page when the submit button is clicked.
 
-submitBtn.on("click", function (event) {
-  event.preventDefault();
-  var searchInput = navSelector.val();
-  getYTApi(searchInput);
-  //console.log(searchInput);
-  // if (!searchInput) {
-  //   return;
-  // }
-  generateCards();
-  introPage.hide();
-  document.getElementById("h2El").innerHTML =
-    "Now showing random " + searchInput + " movies!";
-  document.getElementById("h3El").innerHTML =
-    "Click on a movie to pull up the movie trailer!";
+submitBtn.on("click", function(event) {
+    event.preventDefault();
+    var searchInput = navSelector.val();
+    getYTApi(searchInput);
+    //console.log(searchInput);
+    // if (!searchInput) {
+    //   return;
+    // }
+    generateCards();
+    introPage.hide();
+    document.getElementById("h2El").innerHTML =
+        "Now showing random " + searchInput + " movies!";
+    document.getElementById("h3El").innerHTML =
+        "Click on a movie to pull up the movie trailer!";
 });
 //--------------------------
 
 //---------------------
 //Set fetch for youtube api
 function getYTApi(searchTerm) {
-  // fetch(youtubeUrl).then(function (response) {
-  //   console.log(response);
-  // });
-  fetch(
-    youtubeUrl + searchTerm
-    // "https://developers.google.com/apis-explorer/#p/youtube/v3/youtube.search.list?&key=e615b777f7066471620865b8f7eaf6ab"
-  )
-    .then((res) => res.json())
-    .then((data) => {
-      for (var i = 0; i < data.items.length; i++) {
-        var videoId = data.items[i].id.videoId;
-        //console.log(data.items[i].id.videoId);
-        //https://youtu.be/VIDEOID GOES HERE!!
-        var videoUrl = "https://youtu.be/" + videoId;
-        console.log(videoUrl);
-      }
-      // showMovies(data.items);
-    });
+    // fetch(youtubeUrl).then(function (response) {
+    //   console.log(response);
+    // });
+    fetch(
+            youtubeUrl + searchTerm
+            // "https://developers.google.com/apis-explorer/#p/youtube/v3/youtube.search.list?&key=e615b777f7066471620865b8f7eaf6ab"
+        )
+        .then((res) => res.json())
+        .then((data) => {
+            for (var i = 0; i < data.items.length; i++) {
+                var videoId = data.items[i].id.videoId;
+                //console.log(data.items[i].id.videoId);
+                //https://youtu.be/VIDEOID GOES HERE!!
+                var videoUrl = "https://youtu.be/" + videoId;
+                console.log(videoUrl);
+            }
+            // showMovies(data.items);
+        });
 }
 
 //www.googleapis.com/youtube/v3
@@ -171,7 +171,7 @@ navSelector.on("change", getMovies);
 //---------------------
 //Set fetch for youtube api
 function getYTApi(youtubeUrl) {
-  fetch(youtubeUrl).then(function (response) {
-    console.log(response);
-  });
+    fetch(youtubeUrl).then(function(response) {
+        console.log(response);
+    });
 }
